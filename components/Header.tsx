005c3dc3bd838724ab88711f838304fa5d9fe0f2
Sara Onval <sara.onval@outlook.com>
2025-01-04 05:01:38 +0100

    Fix structure and add boilerplate code

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Header = () => {
+  const linkClass = "hover:underline";
+
+  return (
+    <header className="py-2 px-2 bg-slate-200">
+      <nav>
+        <ul className="flex gap-2">
+          <li className={linkClass}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={linkClass}>
+            <Link href="/about">About</Link>
+          </li>
+          <li className={linkClass}>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li className={linkClass}>
+            <Link href="/article">Article</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
