ab14f9b0308549dcfdc5ef85e8708d20ece44453
MosesDaCode <chammas.mousa1@gmail.com>
2025-01-04 18:10:31 +0100

    Added fonts, typography and startpage.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,20 @@
 import type { Metadata } from "next";
+import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+
+export const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: '700'
+})
+
+export const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: "400"
+})
 
 export const metadata: Metadata = {
   title: "AIM - Arameans In Medicine",
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,10 @@
-import ArameansInMedicinePngLogo from "@/assets/ArameansInMedicine";
-import Link from "next/link";
+import { montserrat } from "@/app/layout";
 import Navbar from "./Navbar";
 
 const Header = () => {
-  const linkClass = "hover:underline";
-
   return (
-    <header className="py-2 px-2 bg-slate-200">
+    <header className={`${montserrat.className} h-16 py-2 px-2 bg-slate-200`}>
       <Navbar />
-      {/* <ArameansInMedicinePngLogo />
-      <nav>
-        <ul className="flex gap-2">
-          <li className={linkClass}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={linkClass}>
-            <Link href="/about">About</Link>
-          </li>
-          <li className={linkClass}>
-            <Link href="/contact">Contact</Link>
-          </li>
-          <li className={linkClass}>
-            <Link href="/article">Article</Link>
-          </li>
-        </ul>
-      </nav> */}
     </header>
   );
 };
