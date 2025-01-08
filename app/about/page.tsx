fb7a5fc0a7b96b25bc8f4c5be478f18abc831e56
Sara Onval <sara.onval@outlook.com>
2025-01-08 18:34:55 +0100

    Add About Me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,119 @@
+import { montserrat, poppins } from "@/assets/Fonts";
+
 export default function About() {
-    return (
-      <section>
-        <h1>About Us</h1>
-        <p>AIM is an organization...</p>
-      </section>
-    );
-  }
+  return (
+    <section>
+      <article className="max-w-prose prose mx-auto lg:prose-xl px-4 lg:pt-8">
+        <h1 className={`${montserrat.className} text-4xl`}>About Us</h1>
+        <div className={`${poppins.className}`}>
+          <p>
+            Arameans In Medicine (AIM) stands for peace and unity, will work for
+            Arameans worldwide and welcomes you to join this organisation,
+            whether you're in the medical field <strong>or not</strong>.
+          </p>
+
+          <p>
+            Let's dive into the meaning of the name{" "}
+            <em>Arameans In Medicine</em> and what issues precaused the startup
+            of the organisation. "Medicine" is defined as the science of health
+            and healing. The <em>World Health Organisation</em> defines health
+            as{" "}
+            <strong>
+              "a state of complete physical, mental and social well-being and
+              not merely the absence of disease or infirmity."
+            </strong>{" "}
+            Collaboration and cooperation among various actors, organizations,
+            and levels are crucial aspects that run through the entire
+            healthcare system. This concerns vocations such as{" "}
+            <em>
+              Biomedical Scientists, Doctors, Opticians, Social workers, Nurses,
+              Audiologist, Speech therapists, Pharmacists, Psychologists,
+              Dentists, Physical therapists, Dieticians, Personal Trainers, Life
+              Coahces
+            </em>{" "}
+            etc.
+          </p>
+
+          <p>
+            The "Arameans" are indigenous people of The Middle
+            East(Mesopotamia), mentioned since over 3000 yrs ago and the biggest
+            semitic ethnic group ever existed.
+          </p>
+
+          <p>
+            Today Arameans are dispersed in a global diaspora due to centuries
+            of persecution in their homeland, and due to the world map drawing
+            excluding Arameans from the right to their own lands, which they
+            still inhabitate until today. The name Aramean is the original
+            ethnic name of the people who lived in several city-states in
+            Middle-East, mainly in Mesopotamia. The Arameans have at times been
+            ruled by other nations in history such as the Assyrians and
+            Babylonians but however remained the vast majority of the
+            inhabitants in those areas while under foreign rule. The Arameans
+            kept their Aramean names and culture and thus preserved their
+            identity despite being occupied by foreign rule. The nations who
+            occupied the Aramean lands later came to fall and seized to exist
+            while the Aramean prevailed.
+          </p>
+
+          <p>
+            The Arameans were among the 1st nations to adopt Christianity, and
+            it remains the biggest religion practised by them, next to Mandaeism
+            and Islam. With the Christianisation, the name Syriac or Syrian
+            Aramean (not to be limited to and confused with modern day Syria)
+            gained popularity amongst Christian Arameans for the distinction
+            from Non-Christian Arameans and remains a synonym for Christian
+            Arameans yet today.
+          </p>
+
+          <p>
+            Fast forward a couple of thousand years, The Aramean Christians
+            faced ecclesiastical division, adopting new names (some which
+            already belonged to pre-existing extinct ethnic nations in history,
+            and is not to be confused ethnically with them). Today the Arameans
+            go under the following names:{" "}
+            <strong>
+              Syriac, Maronite, Chaldean, Assyrian, Melkite and Mandean
+            </strong>
+            .
+          </p>
+
+          <p>
+            Arameans speak the language that Jesus spoke, yet their language is
+            on the verge of extinction today. Arameans don't have a state of
+            their own to stand up for their rights or speak up for them.
+            Resulting in the need of organisation to protect them and help fight
+            their extinction. Arameans don't have the same access to public
+            health knowledge in the diaspora as in their homeland due to
+            linguistic, cultural and socioeconomic barriers. In the homeland the
+            barrier is unfortunatelly often poor economic status and racial
+            discrimination. Aramean students in the Medical fields need guidance
+            and the professionels a forum to grow.
+          </p>
+
+          <p>
+            Arameans in Medicine aims to gather medical professionals and
+            students for the cause of helping the Aramean community world wide
+            from a health care point of view. The organisation{" "}
+            <strong>
+              welcomes everyone in the community outside of the medical and
+              health care industry
+            </strong>{" "}
+            to become a benefitting member and supporter, no matter occupation.
+          </p>
+
+          <h3>Christians of Middle East</h3>
+
+          <p>
+            Such as Armenian and Coptic brothers and sisters have a long shared
+            history of being persecuted for their faith and share similar values
+            and struggles in the Middle East. They are warlmy welcomed to join
+            the cause and work for shared goals.
+          </p>
+
+          <p>~AIM~</p>
+        </div>
+      </article>
+    </section>
+  );
+}
