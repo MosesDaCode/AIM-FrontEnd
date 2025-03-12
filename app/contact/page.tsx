773738e347853506064e7ca49c05984abcfd481f
Sara Onval <sara.onval@outlook.com>
2025-03-12 20:53:23 +0100

    Fix lint

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,17 +5,19 @@ export default function Contact() {
     <section>
       <article className="max-w-prose prose mx-auto lg:prose-xl px-4 lg:pt-8 py-4">
         <h1 className={`${montserrat.className} text-4xl`}>Contact Us</h1>
-        <p>
-          <em>Do you want to get in contact with us?</em>
-        </p>
-        <p>Email: info@aim-org.net</p>
-        <hr />
-        <p>
-          <em>For updates, follow us on:</em>
-        </p>
-        <p>Instagram: @Arameans_in_medicine</p>
-        <p>Facebook: @Arameans in medicine</p>
-        <p>LinkedIn: @Arameans in medicine</p>
+        <div className={`${poppins.className}`}>
+          <p>
+            <em>Do you want to get in contact with us?</em>
+          </p>
+          <p>Email: info@aim-org.net</p>
+          <hr />
+          <p>
+            <em>For updates, follow us on:</em>
+          </p>
+          <p>Instagram: @Arameans_in_medicine</p>
+          <p>Facebook: @Arameans in medicine</p>
+          <p>LinkedIn: @Arameans in medicine</p>
+        </div>
       </article>
     </section>
   );
