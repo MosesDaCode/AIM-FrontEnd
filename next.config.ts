b03ce74df25fd803123912bcf1e2ffd127a11a8e
Sara Onval <sara.onval@outlook.com>
2025-03-12 23:16:06 +0100

    Fix production build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // enable export to out folder, which is used for ftp server
+  images: {
+    unoptimized: true, // disable image optimization to make images work in production
+  },
 };
 
 export default nextConfig;
